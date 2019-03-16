77021efb806a17a4554d078ff784791eaf134edf
Stephanie Smith <stephsmithcode@gmail.com>
2019-03-16 16:48:24 -0700

    persisting global state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React, {  } from 'react';
+import React, { useState, useReducer } from 'react';
 import Routes from './routes';
+import Context from './utils/context';
+import { HooksReducer, initialState } from './store/hooksState/hooksReducer';
+import { success, failure } from './store/actions/actions';
 
 const App = () => {
+
+  const [stateGlobal, setStateGlobal] = useState(0);
+
+  const [stateContextGlobal, dispatchContextGlobal] = useReducer(HooksReducer, initialState);
+
+  const incrementGlobalState = () => {
+    setStateGlobal(stateGlobal + 1);
+  };
+
+  const decrementGlobalState = () => {
+    setStateGlobal(stateGlobal + 1);
+  };
+
+  const handleContextDispatchTrue = () => {
+    dispatchContextGlobal(success());
+  };
+
+  const handleContextDispatchFalse = () => {
+    dispatchContextGlobal(failure());
+  };
 
   return (
     <div>
       React
-      <Routes />
+      <Context.Provider
+        value={{
+          valueGlobalState: stateGlobal,
+          addGlobalValue: () => incrementGlobalState(),
+          decGlobalValue: () => decrementGlobalState(),
+
+          reducerGlobalState: stateContextGlobal.stateprop2,
+          dispatchContextTrue: () => handleContextDispatchTrue(),
+          dispatchContextFalse: () => handleContextDispatchFalse()
+        }}>
+        <Routes />
+      </Context.Provider>
     </div>
   );
 };
--- src/hooks/HooksContainer1.js
+++ src/hooks/HooksContainer1.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 import { HooksReducer, initialState } from '../store/hooksState/hooksReducer';
 import { success, failure } from '../store/actions/actions';
+import Context from '../utils/context';
 
 const HooksContainer1 = () => {
+  const context = useContext(Context);
 
   const [stateValue, setValue] = useState(0);
   const [useEffectValue, setUseEffectValue] = useState(null);
@@ -39,13 +41,23 @@ const HooksContainer1 = () => {
       <br/>
       <button onClick={() => incrementValue()}>+1</button>
       <button onClick={() => decrementValue()}>-1</button>
+      <br/>
       <button onClick={() => changeUseEffectValue()}>CHANGE</button>
+      <br/>
       <button onClick={() => handleDispatchTrue()}>Dispatch True</button>
       <button onClick={() => handleDispatchFalse()}>Dispatch False</button>
+      <br/>
+      <button onClick={() => context.addGlobalValue()}>+1 Global</button>
+      <button onClick={() => context.decGlobalValue()}>-1 Global</button>
+      <br/>
+      <button onClick={() => context.dispatchContextTrue()}>Dispatch True Global</button>
+      <button onClick={() => context.dispatchContextFalse()}>Dispatch False Global</button>
       <div>
         {useEffectValue ? <p>{useEffectValue}</p> : <p>No Value</p>}
         {state.stateprop1 ? <p>TRUE</p> : <p>FALSE</p>}
+        {context.reducerGlobalState ? <p>TRUE Global</p> : <p>FALSE Global</p>}
         <p>Local State: {stateValue}</p>
+        <p>Global State: {context.valueGlobalState}</p>
       </div>
     </div>
   );
