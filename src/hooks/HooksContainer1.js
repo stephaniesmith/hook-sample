20239317e55cc275a04f02f79cf8b2d75bd51333
Stephanie Smith <stephsmithcode@gmail.com>
2019-03-16 13:02:15 -0700

    fixes

--- src/hooks/HooksContainer1.js
+++ src/hooks/HooksContainer1.js
@@ -1,7 +1,7 @@
 import React, {  } from 'react';
 import Routes from './routes';
 
-const App = () => {
+const HooksContainer1 = () => {
 
   return (
     <div>
@@ -11,4 +11,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default HooksContainer1;
