d6a12377526c473c995ec40aee7bc045f6330e94
Stephanie Smith <stephsmithcode@gmail.com>
2019-03-16 16:08:15 -0700

    reducer

--- src/hooks/HooksContainer1.js
+++ src/hooks/HooksContainer1.js
@@ -1,9 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
+import { HooksReducer, initialState } from '../store/hooksState/hooksReducer';
+import { success, failure } from '../store/actions/actions';
 
 const HooksContainer1 = () => {
 
   const [stateValue, setValue] = useState(0);
   const [useEffectValue, setUseEffectValue] = useState(null);
+
+  const [state, dispatch] = useReducer(HooksReducer, initialState);
 
   useEffect(() => {
     setTimeout(() => setUseEffectValue('use effect work'), 3000);
@@ -21,6 +25,14 @@ const HooksContainer1 = () => {
     setUseEffectValue('Some string');
   };
 
+  const handleDispatchTrue = () => {
+    dispatch(success());
+  };
+
+  const handleDispatchFalse = () => {
+    dispatch(failure());
+  };
+
   return (
     <div>
       React Hooks
@@ -28,8 +40,11 @@ const HooksContainer1 = () => {
       <button onClick={() => incrementValue()}>+1</button>
       <button onClick={() => decrementValue()}>-1</button>
       <button onClick={() => changeUseEffectValue()}>CHANGE</button>
+      <button onClick={() => handleDispatchTrue()}>Dispatch True</button>
+      <button onClick={() => handleDispatchFalse()}>Dispatch False</button>
       <div>
         {useEffectValue ? <p>{useEffectValue}</p> : <p>No Value</p>}
+        {state.stateprop1 ? <p>TRUE</p> : <p>FALSE</p>}
         <p>Local State: {stateValue}</p>
       </div>
     </div>
